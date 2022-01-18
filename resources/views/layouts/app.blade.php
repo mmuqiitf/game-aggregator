b4536c953c2e4ed30ec96e17aaf2b983c016bcb3
Muqiit <mmuqiit.f14@gmail.com>
2022-01-18 15:14:14 +0700

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laracasts Video Games</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+
+<body class="bg-gray-900 text-white antialiased">
+    <header class="border-b border-gray-800">
+        <nav class="container mx-auto flex flex-col lg:flex-row items-center justify-between px-4 py-6">
+            <div class="flex flex-col lg:flex-row items-center">
+                <a href="/">
+                    <img src="{{ asset('image/laracasts-logo.svg') }}" alt="laracasts" class="w-32 flex-none">
+                </a>
+                <ul class="flex ml-0 lg:ml-16 mt-6 lg:mt-0 space-x-8">
+                    <li><a href="#" class="hover:text-gray-400">Games</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Coming Soon</a></li>
+                </ul>
+            </div>
+            <div class="flex items-center mt-6 lg:mt-0">
+                <div class="relative">
+                    <input type="text" name=""
+                        class="bg-gray-800 text-sm rounded-full px-3 pl-8 py-1 w-64 focus:outline-none focus:shadow-outline"
+                        placeholder="Search..." id="">
+                    <div class="absolute top-0 flex items-center h-full ml-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                        </svg>
+                    </div>
+                </div>
+                <div class="ml-6">
+                    <a href="#">
+                        <img src="{{ asset('image/fun.svg') }}" alt="avatar" class="rounded-full w-8">
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main class="py-8">
+        @yield('content')
+    </main>
+
+    <footer class="border-t border-gray-800">
+        <div class="container mx-auto px-4 py-6">
+            Powered by <a href="" class="underline hover:text-gray-400">IGDB API</a>
+        </div>
+    </footer>
+</body>
+
+<html>
